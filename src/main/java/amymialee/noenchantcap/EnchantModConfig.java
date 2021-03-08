79bb719e5852dbd19570d926fef74ea42948588e
AmyMialee <10999535+AmyMialee@users.noreply.github.com>
2021-03-08 23:08:38 +0000

    Config and /enchant

--- src/main/java/amymialee/noenchantcap/EnchantModConfig.java
+++ src/main/java/amymialee/noenchantcap/EnchantModConfig.java
@@ -6,6 +6,46 @@ import me.shedaniel.autoconfig.annotation.Config;
 @Config(name = "noenchantcap")
 public
 class EnchantModConfig implements ConfigData {
-    public int enchantCap = 10;
     public boolean removeAnvilLimit = true;
+    public boolean allowAllEnchantmentCombinations = false;
+
+    public boolean useGlobalEnchantCap = true;
+    public int globalEnchantCap = 10;
+
+    public int protectionCap = 4;
+    public int protectionFireCap = 4;
+    public int protectionBlastCap = 4;
+    public int protectionProjectileCap = 4;
+
+    public int respirationCap = 3;
+    public int thornsCap = 3;
+    public int frostWalkerCap = 2;
+    public int soulSpeedCap = 3;
+    public int depthStriderCap = 3;
+    public int featherFallingCap = 4;
+
+    public int smiteCap = 5;
+    public int sharpnessCap = 5;
+    public int baneOfArthropodsCap = 5;
+    public int knockbackCap = 2;
+    public int fireAspectCap = 2;
+    public int lootingCap = 3;
+    public int sweepingCap = 3;
+
+    public int efficiencyCap = 5;
+    public int fortuneCap = 3;
+
+    public int powerCap = 5;
+    public int punchCap = 2;
+
+    public int lureCap = 3;
+    public int luckOfTheSeaCap = 3;
+
+    public int impalingCap = 5;
+    public int loyaltyCap = 3;
+
+    public int quickChargeCap = 3;
+    public int piercingCap = 4;
+
+    public int unbreakingCap = 3;
 }
--- src/main/java/amymialee/noenchantcap/mixin/AnvilScreenHandlerMixin.java
+++ src/main/java/amymialee/noenchantcap/mixin/AnvilScreenHandlerMixin.java
@@ -17,14 +17,84 @@ public class AnvilScreenHandlerMixin {
         if (enchantment.getMaxLevel() == 1) {
             return 1;
         } else {
-            return config.enchantCap;
+            if (config.useGlobalEnchantCap) {
+                return config.globalEnchantCap;
+            } else {
+                switch (enchantment.getTranslationKey()) {
+                    case "enchantment.minecraft.sharpness":
+                        return config.sharpnessCap;
+                    case "enchantment.minecraft.smite":
+                        return config.smiteCap;
+                    case "enchantment.minecraft.bane_of_arthropods":
+                        return config.baneOfArthropodsCap;
+                    case "enchantment.minecraft.knockback":
+                        return config.knockbackCap;
+                    case "enchantment.minecraft.fire_aspect":
+                        return config.fireAspectCap;
+                    case "enchantment.minecraft.sweeping":
+                        return config.sweepingCap;
+                    case "enchantment.minecraft.protection":
+                        return config.protectionCap;
+                    case "enchantment.minecraft.fire_protection":
+                        return config.protectionFireCap;
+                    case "enchantment.minecraft.feather_falling":
+                        return config.featherFallingCap;
+                    case "enchantment.minecraft.blast_protection":
+                        return config.protectionBlastCap;
+                    case "enchantment.minecraft.projectile_protection":
+                        return config.protectionProjectileCap;
+                    case "enchantment.minecraft.respiration":
+                        return config.respirationCap;
+                    case "enchantment.minecraft.depth_strider":
+                        return config.depthStriderCap;
+                    case "enchantment.minecraft.frost_walker":
+                        return config.frostWalkerCap;
+                    case "enchantment.minecraft.soul_speed":
+                        return config.soulSpeedCap;
+                    case "enchantment.minecraft.efficiency":
+                        return config.efficiencyCap;
+                    case "enchantment.minecraft.unbreaking":
+                        return config.unbreakingCap;
+                    case "enchantment.minecraft.looting":
+                        return config.lootingCap;
+                    case "enchantment.minecraft.fortune":
+                        return config.fortuneCap;
+                    case "enchantment.minecraft.luck_of_the_sea":
+                        return config.luckOfTheSeaCap;
+                    case "enchantment.minecraft.lure":
+                        return config.lureCap;
+                    case "enchantment.minecraft.power":
+                        return config.powerCap;
+                    case "enchantment.minecraft.punch":
+                        return config.punchCap;
+                    case "enchantment.minecraft.thorns":
+                        return config.thornsCap;
+                    case "enchantment.minecraft.loyalty":
+                        return config.loyaltyCap;
+                    case "enchantment.minecraft.impaling":
+                        return config.impalingCap;
+                    case "enchantment.minecraft.quick_charge":
+                        return config.quickChargeCap;
+                    default:
+                        return config.globalEnchantCap;
+                }
+            }
         }
+    }
+
+    @Redirect(method = "updateResult", at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/enchantment/Enchantment;canCombine(Lnet/minecraft/enchantment/Enchantment;)Z"))
+    private boolean redirectCanCombine(Enchantment enchantment, Enchantment enchantment2) {
+        if (config.allowAllEnchantmentCombinations) {
+            return true;
+        }
+        return enchantment.canCombine(enchantment2);
     }
 
     @ModifyConstant(method = "updateResult", constant = @Constant(intValue = 40, ordinal = 2))
     private int modifyMaxCost(int original) {
         if (config.removeAnvilLimit) {
-            return 500000;
+            return 50000;
         } else {
             return original;
         }
