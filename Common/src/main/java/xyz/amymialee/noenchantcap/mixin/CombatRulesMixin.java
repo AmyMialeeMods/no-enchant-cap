0dd73914a4e81e78aa7e4b548b77e2dc55765097
AmyMialee <10999535+AmyMialee@users.noreply.github.com>
2022-12-16 13:00:36 +0000

    feat: uncapped prot

---
+++ Common/src/main/java/xyz/amymialee/noenchantcap/mixin/CombatRulesMixin.java
@@ -0,0 +1,15 @@
+package xyz.amymialee.noenchantcap.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.world.damagesource.CombatRules;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(CombatRules.class)
+public class CombatRulesMixin {
+    @WrapOperation(method = "getDamageAfterMagicAbsorb", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Mth;clamp(FFF)F"))
+    private static float noEnchantCap$uncappedCommand(float variable, float min, float max, Operation<Float> original) {
+        return variable;
+    }
+}
