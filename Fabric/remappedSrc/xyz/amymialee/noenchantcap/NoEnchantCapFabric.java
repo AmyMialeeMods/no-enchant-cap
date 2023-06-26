cd26b6f2d85b97767ed351adae867d1af2ae2c90
TheBluekr <kasper.ruhe@gmail.com>
2023-06-26 23:06:23 +0200

    Migrate mappings

---
+++ Fabric/remappedSrc/xyz/amymialee/noenchantcap/NoEnchantCapFabric.java
@@ -0,0 +1,22 @@
+package xyz.amymialee.noenchantcap;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.gamerule.v1.CustomGameRuleCategory;
+import net.minecraft.text.Text;
+
+/**
+ * Initializes the custom gamerule category and the common initializer.
+ */
+public class NoEnchantCapFabric implements ModInitializer {
+    /**
+     * Custom Gamerules category for the mod on fabric. Uses players category on forge due to lack of custom gamerule categories.
+     */
+    public static final CustomGameRuleCategory ENCHANT_CAP_GAMERULES = new CustomGameRuleCategory(NoEnchantCap.id("no_enchant_cap"), Text.translatable("gamerule.category."+ NoEnchantCap.MOD_ID));
+
+    @Override
+    public void onInitialize() {
+        NoEnchantCap.init();
+        CommandRegistrationCallback.EVENT.register((commandDispatcher, commandBuildContext, commandSelection) -> EnchantedBookCommand.register(commandDispatcher, commandBuildContext));
+    }
+}
